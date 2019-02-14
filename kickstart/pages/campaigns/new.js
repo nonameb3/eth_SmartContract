1432edfa8ac1b6c6c0fa11a1e8cdeb6d0e0a2f17
waraphon roonnapai <eepw0211@gmail.com>
2019-02-14 19:59:14 +0700

    add form

--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+import { Button, Form, Input, Grid } from 'semantic-ui-react'
+import Layout from '../../components/Layout'
 
 export class New extends Component {
+  state = {
+    minimumContribution: ''
+  }
+
   render() {
     return (
-      <div>
-        New Page
-      </div>
+      <Layout>
+        <Grid centered columns={2}>
+          <Grid.Column>
+            <h3>Create a Campaign.</h3>
+            <Form>
+              <Form.Field>
+                <lable>Minimum Contribution </lable>
+                <Input
+                  label="WEI"
+                  labelPosition="right"
+                  value={this.state.minimumContribution}
+                  onChange={event=>this.setState({minimumContribution:event.target.value})}
+                />
+              </Form.Field>
+                <Button primary>Create</Button>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </Layout>
     )
   }
 }
--- kickstart/pages/index.js
+++ kickstart/pages/index.js
@@ -26,10 +26,7 @@ class index extends React.Component{
     return(
       <div>
         <Layout>
-          <link
-            rel="stylesheet"
-            href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
-          />
+          
           <h3>Open Campaign</h3>
           <Button floated="right" content="Create Campaign" icon="add circle" primary/>
           {this.randerCampaigns()}
